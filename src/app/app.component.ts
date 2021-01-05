af7402628a9949452070215e3187510af12d9129
James Spurlin <spurlinjames@gmail.com>
2021-01-04 18:35:45 -0600

    Added highlighted space debris

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { JsonPipe } from '@angular/common';
 import { Component } from '@angular/core';
 import { Satellite } from './satellite';
 
@@ -11,12 +12,25 @@ export class AppComponent {
   sourceList: Satellite[];
 
   constructor() {
-    this.sourceList = [
-      new Satellite("SiriusXM", "Communication", "2009-03-21", "LOW", true),
-      new Satellite("Cat Scanner", "Imaging", "2012-01-05", "LOW", true),
-      new Satellite("Weber Grill", "Space Debris", "1996-03-25", "HIGH", false),
-      new Satellite("GPS 938", "Positioning", "2001-11-01", "HIGH", true),
-      new Satellite("ISS", "Space Station", "1998-11-20", "LOW", true),
-    ];
+    this.sourceList = [];
+    let satellitesUrl = 'https://handlers.education.launchcode.org/static/satellites.json';
+ 
+    window.fetch(satellitesUrl).then(function(response) {
+       response.json().then(function(data) {
+ 
+          let fetchedSatellites = data.satellites;
+          for (let i = 0; i < fetchedSatellites.length; i++) {
+            let newSatellite = new Satellite(
+              fetchedSatellites[i].name,
+              fetchedSatellites[i].type,
+              fetchedSatellites[i].launchDate,
+              fetchedSatellites[i].orbitType,
+              fetchedSatellites[i].operational,
+            );
+            this.sourceList.push(newSatellite);
+            this.displayList = this.sourceList.slice(0);
+          }
+        }.bind(this));
+    }.bind(this));
   }
 }
